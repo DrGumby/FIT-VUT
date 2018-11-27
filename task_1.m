f2b9802603f48d3de382e926888447da12639694
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2018-11-27 14:24:15 +0100

    Exercise 2 Done

--- task_1.m
+++ task_1.m
@@ -1,13 +1,7 @@
 %Read wav file
+ 
 
-[pathstr, name, ext] = fileparts(mfilename('fullpath'));
-location = pwd;
-if(location != pathstr)
-  cd pathstr;
-endif
-  
-
-[s, Fs] = wavread('xvojan00.wav');
+[s, Fs] = audioread('xvojan00.wav');
 s = s';
 time = length(s)/Fs;
 binary_count = length(s)/16;
@@ -20,7 +14,7 @@ fprintf(stdout, "The binary number count is: %d\n", binary_count);
 
 binary_j = 1;
 binary_vector = [];
-for i = 1:8:32000
+for i = 8:16:32000
   if (s(i) < 0)
     binary_vector(binary_j) = 0;
   else
@@ -28,5 +22,24 @@ for i = 1:8:32000
   endif
   binary_j = binary_j+1;
 end
-textfile = load('xvojan00.txt')
+textfile = load('xvojan00.txt');
+textfile = textfile';
+if (isequal(textfile, binary_vector))
+  fprintf(stdout, "The binary vectors are equal\n");
+else
+  fprintf(stdout, "The binary vectors are not equal\n");
+endif
 
+
+scale = 1:(Fs*20e-3);
+bin_Fs = Fs/16;
+bin_scale = 1:(bin_Fs*20e-3);
+bin_sig20 = binary_vector(bin_scale);
+sig20 = s(scale);
+bin_t = linspace(0, length(bin_sig20)/bin_Fs, length(bin_sig20));
+t = linspace(0, length(sig20)/Fs, length(sig20));
+
+plot(t, sig20);xlabel('t'); ylabel('s[n], symbols'); title('Exercise 2');
+hold on;
+stem(bin_t, bin_sig20);
+hold off;
